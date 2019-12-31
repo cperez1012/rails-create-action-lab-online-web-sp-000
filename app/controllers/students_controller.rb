0f690ab4a6b3b6ee8fe666aa77fac75e028756a0
Christian Perez <perezchristian1012@gmail.com>
2019-12-31 15:01:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,11 @@ class StudentsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new
+    @post.first_name = params[:first_name]
+    @post.last_name = params[:last_name]
+    @post.save
+    redirect_to post_path(@post)
   end
 
 end
